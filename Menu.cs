69dc73bd6f44980719c4563a7046d7331c67d5d5
FirePalace <gseid55@gmail.com>
2024-01-08 16:23:29 +0100

    Menu

--- Menu.cs
+++ Menu.cs
@@ -3,13 +3,11 @@ using System;
 
 public partial class Menu : Control
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	private void _on_play_pressed(){
+		GetTree().ChangeSceneToFile("res://ChessScenes/node_2d.tscn");
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	private void _on_quit_pressed(){
+		GetTree().Quit();
 	}
+	
 }
